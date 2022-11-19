ce31fbe80d67f0633a89ef1fa3f76dfb7607df4d
Iakov <project3@panargin.ru>
2022-11-19 15:44:22 +0300

    add example006 from max search lesson 2

--- example004/Program.cs
+++ example004/Program.cs
@@ -1,7 +1,6 @@
-﻿Console.WriteLine("2 Hello, World!");
-Console.Write("Введите первое число:");
-double a = int.Parse(Console.ReadLine());
+﻿Console.Write("Введите первое число:");
+double a = Convert.ToDouble(Console.ReadLine());
 Console.Write("Введите второе число:");
-double b = int.Parse(Console.ReadLine());
-double result = a/b;
-Console.WriteLine ("Результат сложения двух чисел: " + result);
+double b = Convert.ToDouble(Console.ReadLine());
+double result = a / b;
+Console.WriteLine("Результат сложения двух чисел: " + result);
