05af546f2f199ceb2140a43b3e47c3e7557fb834
Iakov <project3@panargin.ru>
2022-11-19 15:35:15 +0300

    add example006 from max search lesson

---
+++ example006/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 8;
+int b = 12;
+int c = 1;
+int d = 3;
+int e = 92;
+int f = 10;
+
+int max = a;
+
+if (max < b) max = b;
+if (max < c) max = c;
+if (max < d) max = d;
+if (max < e) max = e;
+if (max < f) max = f;
+
+Console.WriteLine(max);
