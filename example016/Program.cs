4420e57fa5fe853529ec2e3fe21e43c5782055ce
Iakov <project3@panargin.ru>
2022-12-06 21:32:18 +0300

    lesson004

---
+++ example016/Program.cs
@@ -0,0 +1,9 @@
+﻿string[,] table = new string[2, 5];
+table[1, 2] = "слово";
+for (int rows = 0; rows < 2; rows++)
+{
+    for (int columns = 0; columns < 5; columns++)
+    {
+        Console.WriteLine($" - {table[rows, columns]} -");
+    }
+}
---
+++ example018/Program.cs
@@ -0,0 +1,23 @@
+﻿double Factorial(int n) //Что бы не было переполнения используем тип double
+{
+ // 1! = 1
+ // 0! = 1
+ if(n == 1) return 1;
+ else return n * Factorial(n-1);
+}
+for (int i = 1; i < 40; i++)
+{
+ Console.WriteLine($"{i}! = {Factorial(i)}");
+}
+
+
+
+double Fibonacci(int n)
+{
+ if(n == 1 || n == 2) return 1;
+ else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+for (int i = 1; i < 50; i++)
+{
+ Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+}
